2a416e429c1988938ab0e3e3c2869f23017ae8f5
Li Haomin <lihaomin@tp-link.com.cn>
2017-03-17 09:03:47 +0800

    fixed newline issues.

--- PrdDeployer/runstops.py
+++ PrdDeployer/runstops.py
@@ -25,6 +25,7 @@ instance = EC2Instance.objects.get(pk=8)
 """
 
 def main():
+    """Multi-thread (parallel) stopping."""
     for module in Module.objects.all():
         ec2instances = module.instances.filter(service_status__in=('to_stop', 'stopped'))
         #ec2instances = module.instances.all()
@@ -43,5 +44,26 @@ def main():
         for runner in runners:
             runner.join()
 
+
+def main1():
+    """Serial stopping."""
+    for module in Module.objects.all():
+        ec2instances = module.instances.filter(service_status__in=('to_stop', 'stopped'))
+        stoppers = []
+        for ec2instance in ec2instances:
+            stopper = EC2Stopper(module,
+                                 ec2instance,
+                                 settings.PEM_DIR,
+                                 settings.SERVICE_TYPES,
+                                 settings.TIME_ZONE,
+                                 settings.STOP_TIMEOUT)
+            stoppers.append(stopper)
+
+        for stopper in stoppers:
+            #actions, cmds = stopper.assemble_stop_cmd()
+            #print(cmds)
+            results = stopper.run_stop_commands()
+            print(results)
+
 if __name__ == "__main__":
-    main()
+    main1()
