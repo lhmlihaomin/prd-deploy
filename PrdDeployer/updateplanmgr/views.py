a3e39882b5f3ecbe762cd69eda0bd9d67e0bd860
Li Haomin <lhmlihaomin@live.com>
2019-08-05 17:24:17 +0800

    fix: multiple images have the same <mod-ver>

--- PrdDeployer/updateplanmgr/views.py
+++ PrdDeployer/updateplanmgr/views.py
@@ -136,12 +136,12 @@ def updateplan(request, plan_id):
 
 def get_module_image(profile, region, module_name, version):
     """Get module AMI by module name and version"""
-    image = AWSResource.objects.get(
+    image = AWSResource.objects.filter(
         profile=profile,
         region=region,
         resource_type="image",
         name__contains="-%s-%s-"%(module_name, version)
-    )
+    ).order_by('-name').first()
     return image
 
 
@@ -149,9 +149,8 @@ def make_new_module(profile, region, module_name, version, \
     instance_count, configuration, service_type, load_balancer_names):
     """Create a brand new module"""
     # Check for AMI image:
-    try:
-        new_image = get_module_image(profile, region, module_name, version)
-    except:
+    new_image = get_module_image(profile, region, module_name, version)
+    if new_image is None:
         raise Exception("ImageNotFound: %s:%s, %s-%s"%(profile.name, region.name, module_name, version))
     # check for old module:
     query_set = Module.objects.filter(
